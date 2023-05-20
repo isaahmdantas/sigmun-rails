8dc7e64197d28abd98788a5d9c6c6b3b10a801f9
Isadora Dantas <isadoramariadasilvadantas@gmail.com>
2023-05-20 12:32:54 -0300

    Configuração do CSS e JS do sigmun

--- app/assets/template.rb
+++ app/assets/template.rb
@@ -3,3 +3,7 @@ directory "app/assets/images", force: true
 File.rename "app/views/stylesheets/application.css", "app/views/stylesheets/application.scss"
 
 directory "app/assets/stylesheets", force: true
+
+copy_file "app/javascript/application.js"
+
+copy_file "vendor/javascript/sigmun.js"
