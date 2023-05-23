9db8723e315811495bd088f27bb83f73ac7bcca4
Isadora Dantas <isadoramariadasilvadantas@gmail.com>
2023-05-23 16:54:15 -0300

    Flash Headers utilizando o plugin Toastify

--- app/template.rb
+++ app/template.rb
@@ -14,6 +14,30 @@ insert_into_file "app/controllers/application_controller.rb", after: /^class App
     <<-RUBY
     protect_from_forgery prepend: true, with: :exception
     protect_from_forgery prepend: true, with: :null_session, if: proc { |c| c.request.format == 'application/json' }  
+
+    after_action :flash_to_headers
+
+    def flash_to_headers
+        return unless request.xhr?
+        response.headers['X-Message'] = flash_message
+        response.headers["X-Message-Type"] = flash_type.to_s
+        flash.discard # don't want the flash to appear when you reload page
+    end
+
+    private
+        def flash_message
+            [:error, :warning, :notice].each do |type|
+                return flash[type] unless flash[type].blank?
+            end
+        end
+
+        def flash_type
+            [:error, :warning, :notice].each do |type|
+                return type unless flash[type].blank?
+            end
+        end
+
+
     RUBY
 end
 
